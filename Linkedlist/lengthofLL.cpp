be1ea35dec87668fe7c3177ef3a50b2482b11e75
MOHAMMAD AZHAR SOFI <98463584+azharsofi@users.noreply.github.com>
2023-06-14 20:12:26 +0530

    Create lengthofLL.cpp

---
+++ Linkedlist/lengthofLL.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+    public:
+    //Function to count nodes of a linked list.
+    int getCount(struct Node* head){
+    
+        //Code here
+        int l=0;
+        Node* p=head;
+        while(p!=NULL){
+            p=p->next;
+            l++;
+        }
+        return l;
+    
+    }
+};
