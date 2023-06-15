b3a15e5c3ba6158b42f064c692941f298b755945
MOHAMMAD AZHAR SOFI <98463584+azharsofi@users.noreply.github.com>
2023-06-15 21:32:58 +0530

    Update detectaloop.cpp

--- Linkedlist/detectaloop.cpp
+++ Linkedlist/detectaloop.cpp
@@ -1,3 +1,31 @@
+
+
+// using map 
+class Solution
+{
+    public:
+    //Function to check if the linked list has a loop.
+    bool detectLoop(Node* head)
+    {if(head==NULL)
+    return false;
+    Node* p=head;
+    map<Node*,bool> v;
+    while(p!=NULL){
+    if(v[p]==true)
+    return true;
+    v[p]=true;
+    p=p->next;
+    }
+    return false;
+    
+    
+       
+    }
+};
+
+
+
+//using fast and slow pointer concept
 class Solution
 {
     public:
