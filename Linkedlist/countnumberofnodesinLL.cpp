473ea0ea9ceb6459f4acf4b76808f42311f1790c
MOHAMMAD AZHAR SOFI <98463584+azharsofi@users.noreply.github.com>
2023-06-16 15:02:24 +0530

    Update countnumberofnodesinLL.cpp

--- Linkedlist/countnumberofnodesinLL.cpp
+++ Linkedlist/countnumberofnodesinLL.cpp
@@ -1,3 +1,35 @@
+int countNodesinLoop(struct Node *head) {
+    struct Node *slow = head;  // Slow pointer moves one node at a time
+    struct Node *fast = head;  // Fast pointer moves two nodes at a time
+
+    // Move the pointers until they meet or reach the end of the list
+    while (fast != nullptr && fast->next != nullptr) {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        // If the pointers meet, a loop is detected
+        if (slow == fast) {
+            // Count the number of nodes in the loop
+            int count = 1;
+            struct Node *temp = slow->next;
+            while (temp != slow) {
+                count++;
+                temp = temp->next;
+            }
+            return count;
+        }
+    }
+
+    // No loop found
+    return 0;
+}
+
+
+
+//or
+
+
+
 int countNodesinLoop(struct Node *head)
 {
     
