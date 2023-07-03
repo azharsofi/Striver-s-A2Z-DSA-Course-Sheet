c55e25a06f16185ccc32bd3398494a454c46dab5
MOHAMMAD AZHAR SOFI <98463584+azharsofi@users.noreply.github.com>
2023-07-03 17:58:06 +0530

    Update peakelement.cpp

--- BinarySearch/peakelement.cpp
+++ BinarySearch/peakelement.cpp
@@ -60,3 +60,29 @@ class Solution
        
     }
 };
+// binary saerch 
+class Solution {
+public:
+    int findPeakElement(vector<int>&a) {
+        int n=a.size();
+        if(n==1)
+        return 0;
+        if(a[0]>=a[1])
+        return 0;
+        if(a[n-1]>=a[n-2])
+        return n-1;
+        int s=1;
+        int e=n-2;
+        while(s<=e){
+           int m=s+(e-s)/2;
+        
+            if(a[m]>=a[m-1]&&a[m]>=a[m+1])
+                  return m;
+                  else if(a[m-1]>a[m])
+                  e=m-1;
+                  else
+                  s=m+1;
+        } 
+       return -1;
+    }
+};
