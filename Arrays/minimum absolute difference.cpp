13252241bd13f7d72f9d4330c3d2ec042cd2a82e
MOHAMMAD AZHAR SOFI <98463584+azharsofi@users.noreply.github.com>
2023-07-04 16:38:27 +0530

    Create minimum absolute difference.cpp

---
+++ Arrays/minimum absolute difference.cpp
@@ -0,0 +1,24 @@
+//O(n) time complexity
+
+class Solution {
+public:
+    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+         vector<vector<int>> a;
+         
+
+         int n=arr.size();
+         sort(arr.begin(),arr.end());
+            int min=INT_MAX;
+          for(int i=0;i<n-1;i++){
+             if(abs(arr[i]-arr[i+1])<=min)
+             min=abs(arr[i]-arr[i+1]);
+             }
+             for(int i=0;i<n-1;i++){
+             if(abs(arr[i]-arr[i+1])==min)
+             a.push_back({arr[i],arr[i+1]});
+             }
+    
+        
+        return a;
+    }
+};
