bc809fcf755b5a08451aae7d26d6691531774360
MOHAMMAD AZHAR SOFI <98463584+azharsofi@users.noreply.github.com>
2023-07-13 13:13:04 +0530

    Create rearrangearrayelementsbysign.cpp

---
+++ Arrays/rearrangearrayelementsbysign.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> pos;
+        vector<int> neg;
+        for(int i=0;i<n;i++){
+            if(nums[i]>0)
+            pos.push_back(nums[i]);
+            else
+            neg.push_back(nums[i]);
+
+        }
+        for(int i=0;i<n/2;i++){
+            nums[2*i]=pos[i];
+            nums[2*i+1]=neg[i];
+        }
+        return nums;
+    }
+};
+
+//O(n)
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> ans(n,0);
+        int pos=0;
+        int neg=1;
+        for(int i=0;i<n;i++){
+            if(nums[i]<0){
+                ans[neg]=nums[i];
+                neg+=2;
+            }
+            else{
+                ans[pos]=nums[i];
+                pos+=2;
+            }
+        }
+       
+        
+        return ans;
+    }
+}
