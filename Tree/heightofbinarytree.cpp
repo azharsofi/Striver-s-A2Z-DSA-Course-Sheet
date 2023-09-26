da5d3929508f934e8fbfd89536e176d227c2726d
MOHAMMAD AZHAR SOFI <98463584+azharsofi@users.noreply.github.com>
2023-09-26 14:55:45 +0530

    Create heightofbinarytree.cpp

---
+++ Tree/heightofbinarytree.cpp
@@ -0,0 +1,12 @@
+class Solution{
+    public:
+    //Function to find the height of a binary tree.
+    int height(struct Node* node){
+        // code here 
+        if(node==NULL)
+        return 0;
+        int lh=height(node->left);
+        int rh=height(node->right);
+        return max(lh,rh)+1;
+    }
+};
