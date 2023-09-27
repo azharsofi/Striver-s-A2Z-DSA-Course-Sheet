427c2829cf6198344a369af4cc85b2d8514cbe9e
MOHAMMAD AZHAR SOFI <98463584+azharsofi@users.noreply.github.com>
2023-09-27 17:25:45 +0530

    Create diameteroftree.cpp

---
+++ Tree/diameteroftree.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    private:
+    int height(Node* root){
+        if(root==NULL)
+        return 0;
+        int lh=height(root->left);
+        int rh=height(root->right);
+        return max(lh,rh)+1;
+    }
+  public:
+    // Function to return the diameter of a Binary Tree.
+    int diameter(Node* root) {
+        // Your code here
+        if(root==NULL)
+        return 0;
+        int op1=1+height(root->left)+height(root->right);
+        int op2=diameter(root->left);
+        int op3=diameter(root->right);
+        return max(op1,max(op2,op3));
+    }
+};
