a19ecfa16ad6d7113c7cc9853f81795f918ff829
MOHAMMAD AZHAR SOFI <98463584+azharsofi@users.noreply.github.com>
2023-09-29 01:20:46 +0530

    Update diameteroftree.cpp

--- Tree/diameteroftree.cpp
+++ Tree/diameteroftree.cpp
@@ -21,3 +21,31 @@ class Solution {
         return max(op1,max(op2,op3));
     }
 };
+
+//optimized
+class Solution {
+    private:
+    pair<int,int> df(Node* root){
+        if(root==NULL)
+    {
+      pair<int,int> p=make_pair(0,0);
+      return p;
+    }
+        pair<int,int> l =df(root->left);
+        pair<int,int> r =df(root->right);
+      int op1=l.first;
+      int op2=r.first;
+      int op3=l.second+r.second+1;
+      
+      pair<int,int> ans;
+      ans.first= max(op1,max(op2,op3));
+      ans.second=max(l.second,r.second)+1;
+      return ans;
+    }
+  public:
+    // Function to return the diameter of a Binary Tree.
+    int diameter(Node* root) {
+        // Your code here
+        return df(root).first;
+    }
+};
