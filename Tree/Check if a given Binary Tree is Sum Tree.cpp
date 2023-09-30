d5525ce4787a12eab9051f85ef4bd9ba17970bd5
MOHAMMAD AZHAR SOFI <98463584+azharsofi@users.noreply.github.com>
2023-09-30 18:11:06 +0530

    Update Check if a given Binary Tree is Sum Tree.cpp

--- Tree/Check if a given Binary Tree is Sum Tree.cpp
+++ Tree/Check if a given Binary Tree is Sum Tree.cpp
@@ -25,3 +25,38 @@ class Solution
            
     }
 };
+// optimized approach
+class Solution
+{
+    public:
+    pair<bool,int> df(Node* root){
+        if(root==NULL)
+        {
+             pair<bool,int> p=make_pair(true,0);
+             return p;
+        }
+        if(root->left==NULL&&root->right==NULL){
+         pair<bool,int> p2=make_pair(true,root->data);
+         return p2;}
+          pair<bool,int> l=df(root->left);
+           pair<bool,int> r=df(root->right);
+           bool op1=l.first;
+           bool op2=r.first;
+           bool op3=root->data==l.second+r.second;
+            pair<bool,int> ans;
+            if(op1&&op2&&op3){
+                ans.first=true;
+                ans.second=root->data+l.second+r.second;
+            }
+            else
+            ans.first=false;
+            return ans;
+    }
+    
+    
+    bool isSumTree(Node* root)
+    {
+         // Your code here
+       return df(root).first;
+    }
+};
