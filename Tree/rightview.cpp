7f67c5bc57fdc2873c57d5b7c76ee58b9a80ac06
MOHAMMAD AZHAR SOFI <98463584+azharsofi@users.noreply.github.com>
2023-10-02 22:59:41 +0530

    Update rightview.cpp

--- Tree/rightview.cpp
+++ Tree/rightview.cpp
@@ -24,3 +24,26 @@ class Solution
        return ans;
     }
 };
+// recursive
+class Solution
+{
+    public:
+   void  solve(Node* root,vector<int> &ans,int depth){
+       if(root==NULL)
+       return ;
+       if(depth==ans.size()){
+           ans.push_back(root->data);
+       }
+       solve(root->right,ans,depth+1);
+       solve(root->left,ans,depth+1);
+   }
+    //Function to return list containing elements of right view of binary tree.
+    vector<int> rightView(Node *root)
+    {
+       // Your Code here
+       vector<int> ans;
+       
+      solve(root,ans,0);
+       return ans;
+    }
+};
