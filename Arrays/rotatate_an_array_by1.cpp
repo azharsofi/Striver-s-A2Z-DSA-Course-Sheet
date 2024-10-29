b9f95fa51bc57b12849104d713850c9d863e9ef0
MOHAMMAD AZHAR SOFI <98463584+azharsofi@users.noreply.github.com>
2024-10-29 18:08:07 +0530

    Create rotatate_an_array_by1.cpp

---
+++ Arrays/rotatate_an_array_by1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    void rotate(vector<int> &arr) {
+        // code here
+        int n=arr.size();
+        int temp=arr[n-1];
+        for(int i=n-1;i>0;i--){
+            arr[i]=arr[i-1];
+            }
+            
+        arr[0]=temp;
+    }
+};
